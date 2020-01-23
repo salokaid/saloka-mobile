82b4c578226140b4e1711d917df9b9db39027073
andreputra <ap919778@gmail.com>
2020-01-23 14:49:40 -0500

    create onboarding flutter

--- lib/screens/inro_view.dart
+++ lib/screens/inro_view.dart
@@ -1,111 +1,62 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_swiper/flutter_swiper.dart';
-import 'package:flutter_splash_screen/model/intro.dart';
-import 'package:flutter_splash_screen/color_pallette.dart';
 
 class IntroPage extends StatefulWidget {
-  @override
+  IntroPage({Key key}) : super(key: key);
+
   _IntroPageState createState() => _IntroPageState();
 }
 
 class _IntroPageState extends State<IntroPage> {
-  final List<Intro> introList = [
-    Intro(
-      image: "images/icon_search.png",
-      title: "Search",
-      description: "Discover Restaurants offering the best fast food food near you on Foodwa",
-    ),
-    Intro(
-      image: "images/icon_hamburger.png",
-      title: "Order",
-      description: "Our veggie plan is filled with delicious seasonal vegetables, whole grains, beautiful cheeses and vegetarian proteins",
-    ),
-    Intro(
-      image: "images/icon_otw.png",
-      title: "Eat",
-      description: "Food delivery or pickup from local restaurants, Explore restaurants that deliver near you.",
-    ),
-  ]; 
+
+  PageController _pageController;
+
+  void _onScroll(){
+    //print('ssss');
+  }
 
   @override
+  void initState() {
+    _pageController = PageController(
+      initialPage: 0
+    )..addListener(_onScroll);
+    super.initState();
+  }
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Swiper.children(
-        index: 0,
-        autoplay: false,
-        loop: false,
-        pagination: SwiperPagination(
-          margin: EdgeInsets.only(bottom: 20.0),
-          builder: DotSwiperPaginationBuilder(
-            color: ColorPalette.dotColor,
-            activeColor: ColorPalette.dotActiveColor,
-            size: 10.0,
-            activeSize: 10.0,
-          ),
-        ),
-        control: SwiperControl(
-          iconNext: null,
-          iconPrevious: null
-        ),
-        children: _buildPage(context),
+    return new Scaffold(
+      body: new PageView(
+        controller: _pageController,
+        children: <Widget>[
+          makePage(image : 'images/Slider-1.png'),
+          makePage(image : 'images/Slider-2.png'),
+          makePage(image : 'images/Slider-3.png')  
+        ],
       ),
     );
   }
+}
 
-  List<Widget> _buildPage(BuildContext context) {
-    List<Widget> widgets = [];
-    for(int i=0; i<introList.length; i++) {
-      Intro intro = introList[i];
-      widgets.add(
-        Container(
-          padding: EdgeInsets.only(
-            top: MediaQuery.of(context).size.height/6,
-          ),
-          child: ListView(
-            children: <Widget>[
-              Image.asset(
-                intro.image,
-                height: MediaQuery.of(context).size.height/3.5,
-              ),
-              Padding(
-                padding: EdgeInsets.only(
-                  top: MediaQuery.of(context).size.height/12.0,
-                ),
-              ),
-              Center(
-                child: Text(
-                  intro.title,
-                  style: TextStyle(
-                    color: ColorPalette.titleColor,
-                    fontSize: 24.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(
-                  top: MediaQuery.of(context).size.height/20.0,
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.symmetric(
-                  horizontal: MediaQuery.of(context).size.height/20.0,
-                ),
-                child: Text(
-                  intro.description,
-                  style: TextStyle(
-                    color: ColorPalette.descriptionColor,
-                    fontSize: 14.0,
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-            ],
-          ),
-        ),
-      );
-    }
-    return widgets;
-  }
+Widget makePage({image}){
+  return new Container(
+    decoration: BoxDecoration(
+      image: DecorationImage(
+        image: AssetImage(image),
+        fit: BoxFit.cover
+      ),
+    ),
+    child: new Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.centerRight,
+          stops: [0.3, 0.9],
+          colors: [
+            Colors.black.withOpacity(.9),
+            Colors.black.withOpacity(.2)
+          ]
+        )
+      ),
+      
+      
+    ),
+  );
 }
--- lib/spleeshscreen_view.dart
+++ lib/spleeshscreen_view.dart
@@ -19,7 +19,7 @@ class _SplashScreenPageState extends State<SplashScreenPage> {
   }
 
   startSplashScreen() async{
-    var duration = const Duration(seconds: 5);
+    var duration = const Duration(seconds: 2);
     return Timer(duration, (){
       Navigator.of(context).pushReplacement(
         MaterialPageRoute(
@@ -36,8 +36,8 @@ class _SplashScreenPageState extends State<SplashScreenPage> {
       backgroundColor: Colors.white,
       body: new Center(
         child: Image.asset("images/splash.png",
-        width: 220.0,
-        height: 100.0,
+        width: 230.0,
+        height: 130.0,
         ),
       ),
     );
